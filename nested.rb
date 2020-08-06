681a4c21bd49c31b3cba2b34df50c8b328205476
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-06 03:18:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +55,10 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -81,7 +74,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for=>"Ruby",:languages=>["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
@@ -103,7 +97,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    alans_new_info="GUI"
+programmer_hash[:alan_kay][:known_for]=alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +121,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+programmer_hash
 end

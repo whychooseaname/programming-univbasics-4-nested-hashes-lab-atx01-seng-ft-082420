9c049acaf0d7e3bccf00d43da6592a6a56cfb7ac
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-06 03:16:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -97,7 +97,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for]="GUI"
+    alans_new_info="GUI"
+programmer_hash[:alan_kay][:known_for]=alans_new_info
 end
 
 def adding_to_dennis
